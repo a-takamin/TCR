dd6e8457470a77d7f9214fed375b46f9940d1914
a-takamin <takamin0609@gmail.com>
2024-10-16 03:30:35 +0900

    fix health log

--- internal/handler/log_handler.go
+++ internal/handler/log_handler.go
@@ -8,24 +8,35 @@ import (
 
 func LogMiddleWare() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		slog.Info("request metadata",
-			"path", c.Request.URL.Path,
-			"method", c.Request.Method,
-			slog.Group("header",
-				slog.String("Host", c.Request.Header.Get("Host")),
-				slog.String("Content-Type", c.Request.Header.Get("Content-Type")),
-				slog.String("Content-Length", c.Request.Header.Get("Content-Length")),
-				slog.String("Range", c.Request.Header.Get("Range")),
-			),
-		)
+
+		// TODO: もっといい方法がある？
+		if c.Request.URL.Path != "/health" {
+
+			slog.Info("request metadata",
+				"path", c.Request.URL.Path,
+				"method", c.Request.Method,
+				slog.Group("header",
+					slog.String("Host", c.Request.Header.Get("Host")),
+					slog.String("Content-Type", c.Request.Header.Get("Content-Type")),
+					slog.String("Content-Length", c.Request.Header.Get("Content-Length")),
+					slog.String("Range", c.Request.Header.Get("Range")),
+				),
+			)
+
+		}
+
 		c.Next()
 
-		slog.Info("response metadata",
-			slog.Group("header",
-				slog.String("Docker-Content-Digest", c.Writer.Header().Get("Docker-Content-Digest")),
-				slog.String("Range", c.Writer.Header().Get("Range")),
-				slog.String("Content-Range", c.Writer.Header().Get("Content-Range")),
-			),
-		)
+		if c.Request.URL.Path != "/health" {
+
+			slog.Info("response metadata",
+				slog.Group("header",
+					slog.String("Docker-Content-Digest", c.Writer.Header().Get("Docker-Content-Digest")),
+					slog.String("Range", c.Writer.Header().Get("Range")),
+					slog.String("Content-Range", c.Writer.Header().Get("Content-Range")),
+				),
+			)
+		}
+
 	}
 }
